ad58fa358423b5913666cd0561455e8937122306
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-18 00:16:56 +0200

    Initial commit. This is the minimal project with just the server source file.

---
+++ src/main/java/com/innotec/bat/server/control/Server.java
@@ -0,0 +1,15 @@
+public class Server {
+	public static void main(String[] args) {
+		System.out.println("Server started.");
+		System.out.println("Server Stopped.");
+	}
+	// class ClientHandler extends Thread {
+	// 	Socket socket;
+	// 	public ClientHandler(Socket socket) {
+
+	// 	}
+	// 	public void run() {
+
+	// 	}
+	// }
+}
