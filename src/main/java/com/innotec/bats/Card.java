49c0bdd2d88b6baf6bd67d11794e4badc38f6a42
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-18 16:20:54 +0200

    A commit before adding files to remote repo

--- src/main/java/com/innotec/bats/Card.java
+++ src/main/java/com/innotec/bats/Card.java
@@ -1,10 +1,16 @@
 package com.innotec.bats;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by phoenix on 7/18/16.
  */
 public class Card implements Serializable {
+    private String cardNo;
+    private String pin;
+    private List<BankAccount> accounts;
+    public Card() {
 
+    }
 }
