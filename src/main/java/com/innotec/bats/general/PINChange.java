33ecb125e34a47fb5c4bc1e6da420d2ea5101727
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-25 07:06:53 +0200

    PinChange.java: Remove unnecessary attributes & related methods (re: pin2)

--- src/main/java/com/innotec/bats/general/PINChange.java
+++ src/main/java/com/innotec/bats/general/PINChange.java
@@ -3,38 +3,11 @@ package com.innotec.bats.general;
 public class PINChange extends Action
 {
 	private String oldPIN;
-	private String newPIN1;
-	private String newPIN2;
-	
-	public PINChange(String oldPIN, String newPIN1,String newPIN2)
+	private String newPIN;
+
+	public PINChange(String oldPIN, String newPIN)
 	{
 		this.oldPIN = oldPIN;
-		this.newPIN1 = newPIN1;
-		this.newPIN2 = newPIN2;
-	}
-	
-	public boolean validateNewPIN()
-	{
-		if(newPIN1.equals(newPIN2))
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean validateOldPIN()
-	{
-		if(oldPIN.equals(Card.getPin()))
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	public void changePIN()
-	{
-		this.validateOldPIN();
-		this.validateNewPIN();
-		Card.setPin(newPIN1);
+		this.newPIN = newPIN;
 	}
 }
