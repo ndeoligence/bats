78633f0a9d140729b9d6bfade1030da4a3c95908
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-27 11:32:37 +0200

    Re-add Dispenser class and Note class

---
+++ src/main/java/com/innotec/bats/client/atm/model/Dispenser.java
@@ -0,0 +1,51 @@
+package com.innotec.bats.client.atm.model;
+
+import java.util.*;
+
+/**
+ * Created by phoenix on 7/18/16.
+ */
+public class Dispenser {
+    /**
+     * The count of slots of different notes within the dispenser.
+     * Essentially a count of the different possible notes
+     * In the case of ZAR_Note,
+     * this just counts distinct notes from R10 to R200
+     */
+    // static public final int COUNT_SLOT = 5;
+    /**
+     * The number of each note initially. reset() uses this value to figure out how many of each note to reset to.
+     */
+    static private int COUNT_NOTES_DEF = 500;
+    /**
+     * Contains pointers to the notes.
+     */
+    private ArrayList<ArrayList<ZAR_Note>> slots;
+    private static ZAR_Note[] notes = {
+        new ZAR_Note(ZAR_Note.R10),
+        new ZAR_Note(ZAR_Note.R20),
+        new ZAR_Note(ZAR_Note.R50),
+        new ZAR_Note(ZAR_Note.R100),
+        new ZAR_Note(ZAR_Note.R200),
+    };
+    
+    public Dispenser() {
+        reset();
+    }
+    private void reset() {
+        slots = new ArrayList<>(COUNT_SLOT);
+        for (int i = 0; i < COUNT_SLOT; ++i) {
+            slots.add(new ArrayList<ZAR_Note>(COUNT_NOTES_DEF));
+        }
+
+    }
+    public boolean dispense(double amount) {
+        if (amount == (int) amount)
+            return dispense((int) amount);
+        else throw new IllegalArgumentException("Invalid dispense amount");
+    }
+    public boolean dispense(int amount) {
+        return false;
+    }
+    public double getBalance() {return 0.0;}
+}
