13717641a8231f584f3e0aef53eea809b33aa689
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-27 11:36:32 +0200

    Move ZAR_Note into Dispenser. Makes logical sense

--- src/main/java/com/innotec/bats/client/atm/model/Dispenser.java
+++ src/main/java/com/innotec/bats/client/atm/model/Dispenser.java
@@ -28,7 +28,7 @@ public class Dispenser {
         new ZAR_Note(ZAR_Note.R100),
         new ZAR_Note(ZAR_Note.R200),
     };
-    
+
     public Dispenser() {
         reset();
     }
@@ -48,4 +48,36 @@ public class Dispenser {
         return false;
     }
     public double getBalance() {return 0.0;}
+
+    private static class ZAR_Note implements Comparable{
+        public static final int R10 = 10;
+        public static final int R20 = 20;
+        public static final int R50 = 50;
+        public static final int R100 = 100;
+        public static final int R200 = 200;
+        private int value;
+        public ZAR_Note(int value) {
+            if (!(value==R10 || value==R20 || value==R50 || value==R100 || value==R200))
+                throw new IllegalArgumentException("Invalid note value");
+            this.value = value;
+        }
+        public int getValue() {return value;}
+        public int[] getPossibleValues() {
+            return new int[] {R10,R20,R50,R100,R200};
+        }
+        @Override
+        public int compareTo(Object other) {
+            if (! (other instanceof ZAR_Note))
+                throw new IllegalArgumentException("Comparison of incompatible types");
+            return (value-((ZAR_Note) other).value);
+        }
+        @Override
+        public boolean equals(Object other) {
+            if (! (other instanceof ZAR_Note))
+                return false;
+            return value==((ZAR_Note) other).value;
+        }
+        @Override
+        public String toString() {return "R" + Integer.toString(value) + ".00";}
+    }
 }
