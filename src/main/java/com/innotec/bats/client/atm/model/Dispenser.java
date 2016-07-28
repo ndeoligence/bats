6ebad5d2b416d7a130b5fc349126f3ef0d5b2498
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-28 13:01:50 +0200

    Remove ZAR_Note class. Use note constants instead.

--- src/main/java/com/innotec/bats/client/atm/model/Dispenser.java
+++ src/main/java/com/innotec/bats/client/atm/model/Dispenser.java
@@ -1,6 +1,6 @@
 package com.innotec.bats.client.atm.model;
 
-import java.util.*;
+import javax.swing.JOptionPane; // for testing (main)
 
 /**
  * Created by phoenix on 7/18/16.
@@ -10,30 +10,21 @@ public class Dispenser {
      * The number of each note initially. {@code reset()} uses this value to figure out how many of each note to reset to.
      */
     private static int DEF_SLOT_SIZE = 500;
-    /**
-     * Contains pointers to the notes.
-     */
-    private ArrayList<ArrayList<ZAR_Note>> slots;
-    private static ZAR_Note[] notes = {
-        new ZAR_Note(ZAR_Note.R10),
-        new ZAR_Note(ZAR_Note.R20),
-        new ZAR_Note(ZAR_Note.R50),
-        new ZAR_Note(ZAR_Note.R100),
-        new ZAR_Note(ZAR_Note.R200),
+
+    /**Note constants*/
+    public static final int R10=10, R20=20, R50=50, R100=100, R200=200;
+    private static final int[] notesValues = {
+            R10, R20, R50, R100, R200
     };
 
+    private int[] notesCount;
     public Dispenser() {
         reset();
     }
     private void reset() {
-        slots = new ArrayList<>(notes.length);
-        for (int i = 0; i < slots.size(); ++i) {
-            slots.add(new ArrayList<ZAR_Note>(DEF_SLOT_SIZE));
-            for (int j = 0; j < DEF_SLOT_SIZE; ++j) {
-                slots.get(j).add(notes[j]);
-            }
-        }
-
+        notesCount = new int[notesValues.length];
+        for (int i = 0; i < notesCount.length; ++i)
+            notesCount[i] = DEF_SLOT_SIZE;
     }
     public boolean dispense(double amount) { // takes care of user giving a double as argument
         if (amount == (int) amount)
@@ -51,53 +42,52 @@ public class Dispenser {
     public boolean dispense(int amount) {
         if (amount > getBalance())
             return false;
-        int count;
-        for (int i = 0; amount > 0; ++i) {
-
+        if (amount % R10 != 0)
+            return false;
+        int rem = amount;
+        for (int i = notesValues.length-1; i >= 0; --i) {
+            return dispense(amount, notesValues.length-1) == 0;
         }
         return true;
     }
-    public double getBalance() {
-        double total = 0;
-        for (int i = 0; i < notes.length; ++i)
-            total += notes[i].getValue() * slots.get(i).size();
+    private int dispense(int amount, int startIndex) {
+        if (startIndex == -1 || amount == 0) return amount;
+
+        int curNoteValue = notesValues[startIndex];
+        int curNoteCount = notesCount[startIndex];
+
+        if (amount >= curNoteValue) {
+            int multiples = amount/curNoteValue;
+            if (curNoteCount < multiples)
+                multiples = curNoteCount;
+
+            int rem = dispense(amount-curNoteValue*multiples,startIndex-1);
+            if (rem==0) {
+                notesCount[startIndex] -= multiples;
+            } else {
+
+            }
+        } else {
+            return dispense(amount,startIndex-1);
+        }
+    }
+    public int getBalance() {
+        int total = 0;
+        for (int i = 0; i < notesCount.length; ++i)
+            total += notesValues[i] * notesCount[i];
         return total;
     }
 
-    private static class ZAR_Note implements Comparable{
-        public static final int R10 = 10;
-        public static final int R20 = 20;
-        public static final int R50 = 50;
-        public static final int R100 = 100;
-        public static final int R200 = 200;
-        private int value;
-        public ZAR_Note(int value) {
-            if (!(value==R10 || value==R20 || value==R50 || value==R100 || value==R200))
-                throw new IllegalArgumentException("Invalid note value");
-            this.value = value;
-        }
-        public int getValue() {return value;}
-        public static int[] getPossibleValues() {
-            return new int[] {R10,R20,R50,R100,R200};
-        }
-        @Override
-        public int compareTo(Object other) {
-            if (! (other instanceof ZAR_Note))
-                throw new IllegalArgumentException("Comparison of incompatible types");
-            return (value-((ZAR_Note) other).value);
-        }
-        @Override
-        public boolean equals(Object other) {
-            if (! (other instanceof ZAR_Note))
-                return false;
-            return value==((ZAR_Note) other).value;
-        }
-        @Override
-        public String toString() {return "R" + Integer.toString(value) + ".00";}
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) { // for testing
         Dispenser disp = new Dispenser();
-        System.out.println("Dispenser bal = " + disp.getBalance());
+        double amount;
+        while (true) {
+            String ans = JOptionPane.showInputDialog(null,"Enter amount:","");
+            if (ans == null)
+                break;
+            if (!disp.dispense(Double.parseDouble(ans)))
+                System.out.println("Dispenser balance insufficient!");
+            System.out.println("Balance: R" + disp.getBalance() + ".00");
+        }
     }
 }
