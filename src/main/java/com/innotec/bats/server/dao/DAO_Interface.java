7d2b3bcb32fc1720e456f84cc950e3737c37b287
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-28 08:19:08 +0200

    Remove unused public methods

--- src/main/java/com/innotec/bats/server/dao/DAO_Interface.java
+++ src/main/java/com/innotec/bats/server/dao/DAO_Interface.java
@@ -1,7 +1,6 @@
 package com.innotec.bats.server.dao;
 
 import java.util.ArrayList;
-
 import com.innotec.bats.general.*;
 
 public interface DAO_Interface
@@ -12,7 +11,6 @@ public interface DAO_Interface
 	void addEmployee(Employee newEmployee);
 	void addAdmin(AdminCard newAdmin);
 	void addTransaction(Transaction newTransaction);
-//	public void addATM(ATM newATM);
 	void addSavingsAccount(SavingsAccount newSavingsAccount);
 	void addCreditCardAccount(CreditCardAccount newCreditCardAccount);
 	AccountHolder getAccountHolder(String cardNo);
@@ -21,11 +19,9 @@ public interface DAO_Interface
 	Employee getEmployee(String employeeID);
 	AdminCard getAdmin(String adminID);
 	Transaction getTransactionForAccount(String accountID);
-//	public ATM getATM(int atmID);
 	boolean updateCardActivity(boolean cardActivity, String cardNo);
 	boolean changePIN(String newPIN, String cardNo);
 	boolean updateAccountActivity(boolean activity, String accountNo);
 	ArrayList<CurrentAccount> getCurrentAccount(String cardNo);
 	ArrayList<SavingsAccount> getSavingsAccount(String cardNo);
-	
 }
