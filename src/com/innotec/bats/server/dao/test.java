d72a0cc528872860f0468e192bb9c4e7205f0e14
Michael Smith <michaelvoight.smith@gmail.com>
2016-07-29 10:57:37 +0200

    the test class for the DAO_Class and resource folder for the connection

---
+++ src/com/innotec/bats/server/dao/test.java
@@ -0,0 +1,35 @@
+package com.innotec.bats.server.dao;
+
+import java.sql.SQLException;
+
+import com.innotec.bats.general.*;
+
+public class test {
+
+	public static void main(String [] args)
+	{
+		DAO_Class dao = new DAO_Class();
+		ATMAdmin tell = new ATMAdmin("Mark", "Shualt", "1234568734503", "34");
+		AccountHolder holder = new AccountHolder("Melize", "Smite", "9701015029089", "2 Jutinga close", "0834014836");
+		AccountHolderCard card = new AccountHolderCard("503956778694658", "1234", true, "9701015029089");
+		AdminCard cardAdmin = new AdminCard("4593456820349567", "4321", true, "1234568734503");
+		
+		dao.addAccountHolder(holder, "1234568734503");
+		dao.addAccountHolderCard(card);
+		//dao.addTeller(tell);
+		
+		try {
+			dao.addAdmin(cardAdmin);
+			holder = dao.getAccountHolderByidNo("9701015029089");
+			//holder = dao.getAccountHolderByCardNo("5039567843094568");
+			card = dao.getAccountHolderCardByCardNo("503956778694658");
+		//	tell = dao.getATMAdmin("1234568734503");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+		System.out.println(holder);
+		System.out.println(card);
+		//System.out.println(tell);
+	}
+}
