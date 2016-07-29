9d0b4fbd4a502720597bb82e395eccaa6c012ed7
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-07-29 12:09:51 +0200

    Modify AHCard per MIchael's changes - to contain Acc Holder ID number

--- src/main/java/com/innotec/bats/general/AccountHolderCard.java
+++ src/main/java/com/innotec/bats/general/AccountHolderCard.java
@@ -5,12 +5,25 @@ package com.innotec.bats.general;
  */
 public class AccountHolderCard extends Card {
     public static final int CARD_NO_LEN = 16;
-    public AccountHolderCard(String cardNo, String pinNo, boolean active) {
+
+    String accountHolderIdNo;
+    public AccountHolderCard(String cardNo, String pinNo, boolean active, String accountHolderIdNo) {
         super(cardNo, pinNo, active);
+        this.accountHolderIdNo=accountHolderIdNo;
+    }
+    public String getAccountHolderIdNo() {
+        return accountHolderIdNo;
+    }
+
+    public void setAccountHolderIdNo(String accountHolderIdNo) {
+        this.accountHolderIdNo = accountHolderIdNo;
     }
 
     @Override
     public String toString() {
-        return "AccountHolderCard{"+super.toString()+"}";
+        return "AccountHolderCard{" +
+                "cardNo='" + getCardNo() + '\'' +
+                "accountHolderIdNo='" + accountHolderIdNo + '\'' +
+                '}';
     }
 }
