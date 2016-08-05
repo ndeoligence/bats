c83aa4afea7224ad1595d8df0af674a8fa54bec5
James Jacobs <jameskentjacobs@gmail.com>
2016-08-05 09:59:10 +0200

    Monde: Removed files unrelated to james

--- src/com/innotec/bats/server/model/SessionTerminationException.java
+++
@@ -1,9 +0,0 @@
-package com.innotec.bats.server.model;
-/**
- * Created by phoenix on 7/22/16.
- */
-public class SessionTerminationException extends BankException {
-    public SessionTerminationException(String message) {
-        super(message);
-    }
-}
