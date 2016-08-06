3bfd5b76a059c4bde7f9e6fb954d30490e4ffc41
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-08-06 19:46:43 +0200

    Create new Exception

---
+++ server/src/main/java/com/innotec/bats/server/model/BadAccountTypeException.java
@@ -0,0 +1,10 @@
+package com.innotec.bats.server.model;
+
+/**
+ * Created by phoenix on 8/6/16.
+ */
+public class BadAccountTypeException extends BankException {
+    public BadAccountTypeException(String message) {
+        super(message);
+    }
+}
