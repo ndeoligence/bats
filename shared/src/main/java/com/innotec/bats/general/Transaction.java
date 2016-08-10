628f0a9d2892c1961ddd388b9a5888146cad8635
Monde Ngalonkulu <monde.ngalo@gmail.com>
2016-08-10 13:55:03 +0200

    Add file with changes from Ilana (General class)

--- shared/src/main/java/com/innotec/bats/general/Transaction.java
+++ shared/src/main/java/com/innotec/bats/general/Transaction.java
@@ -3,28 +3,42 @@ package com.innotec.bats.general;
 import java.util.Date;
 
 
+
 public class Transaction extends Action
 {
 		private String primAccountNo, ATM_ID;
+		private int type;
 		private double amount;
 		private Date datestamp;
+		private final static int TYPE_WITHDRAWAL = 1;
+		private final static int TYPE_DEPOSIT = 2;
+		private final static int TYPE_TRANSFER = 3;
 		
 		public Transaction (String primAccountNo, double amount)
 		{
 			this.primAccountNo = primAccountNo;
 			this.amount = amount;
 			datestamp = new Date();
-			
 		}
 
 		public String getATM_ID ()
 		{
 			return ATM_ID;
 		}
-		
+
 		public void setATM_ID (String atm_id)
 		{
 			 ATM_ID = atm_id;
+		}
+		
+		public int getType ()
+		{
+			return type;
+		}
+
+		public void setType (int type)
+		{
+			this.type = type;
 		}
 
 		public Date getDatestamp ()
